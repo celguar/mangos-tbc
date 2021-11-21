cb292e3c7eef51ce7bf7f9c0d6f823cf996437c1
celguar <celguar@gmail.com>
2021-11-22 02:38:48 +0300

    Crashfixes
    Fix crash in player relocation
    Fix crash in targeted movement generator log
    Crashfix attempt
    Fix possible crash
    BG: don't let player enter already finished BG
    temp fix for saveall crash
    Fix crash in removeSpell
    Fix crash in pet
    Fix crash in visibility
    BG crashfix
    
    Co-Authored-By: blitzi <289959+blitzi@users.noreply.github.com>

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -1160,20 +1160,26 @@ void Map::PlayerRelocation(Player* player, float x, float y, float z, float orie
         DEBUG_FILTER_LOG(LOG_FILTER_PLAYER_MOVES, "Player %s relocation grid[%u,%u]cell[%u,%u]->grid[%u,%u]cell[%u,%u]", player->GetName(), old_cell.GridX(), old_cell.GridY(), old_cell.CellX(), old_cell.CellY(), new_cell.GridX(), new_cell.GridY(), new_cell.CellX(), new_cell.CellY());
 
         NGridType* oldGrid = getNGrid(old_cell.GridX(), old_cell.GridY());
-        RemoveFromGrid(player, oldGrid, old_cell);
-        if (!old_cell.DiffGrid(new_cell))
-            AddToGrid(player, oldGrid, new_cell);
-        else
-            EnsureGridLoadedAtEnter(new_cell, player);
+        if (oldGrid)
+        {
+            RemoveFromGrid(player, oldGrid, old_cell);
+            if (!old_cell.DiffGrid(new_cell))
+                AddToGrid(player, oldGrid, new_cell);
+            else
+                EnsureGridLoadedAtEnter(new_cell, player);
+        }
 
         NGridType* newGrid = getNGrid(new_cell.GridX(), new_cell.GridY());
-        player->GetViewPoint().Event_GridChanged(&(*newGrid)(new_cell.CellX(), new_cell.CellY()));
+        if (newGrid)
+        {
+            player->GetViewPoint().Event_GridChanged(&(*newGrid)(new_cell.CellX(), new_cell.CellY()));
+        }
     }
 
     player->OnRelocated();
 
     NGridType* newGrid = getNGrid(new_cell.GridX(), new_cell.GridY());
-    if (!same_cell && newGrid->GetGridState() != GRID_STATE_ACTIVE)
+    if (newGrid && !same_cell && newGrid->GetGridState() != GRID_STATE_ACTIVE)
     {
         ResetGridExpiry(*newGrid, 0.1f);
         newGrid->SetGridState(GRID_STATE_ACTIVE);
@@ -1568,7 +1574,6 @@ void Map::AddObjectToRemoveList(WorldObject* obj)
     MANGOS_ASSERT(obj->GetMapId() == GetId() && obj->GetInstanceId() == GetInstanceId());
 
     obj->CleanupsBeforeDelete();                            // remove or simplify at least cross referenced links
-
     i_objectsToRemove.insert(obj);
     // DEBUG_LOG("Object (GUID: %u TypeId: %u ) added to removing list.",obj->GetGUIDLow(),obj->GetTypeId());
 }
@@ -1582,6 +1587,7 @@ void Map::RemoveAllObjectsInRemoveList()
     while (!i_objectsToRemove.empty())
     {
         WorldObject* obj = *i_objectsToRemove.begin();
+
         i_objectsToRemove.erase(i_objectsToRemove.begin());
 
         switch (obj->GetTypeId())
@@ -2161,6 +2167,7 @@ BattleGroundMap::BattleGroundMap(uint32 id, time_t expiry, uint32 InstanceId, ui
 
 BattleGroundMap::~BattleGroundMap()
 {
+    UnloadAll(true);
 }
 
 void BattleGroundMap::Initialize(bool)
@@ -2170,6 +2177,9 @@ void BattleGroundMap::Initialize(bool)
 
 void BattleGroundMap::Update(const uint32& diff)
 {
+    if (!GetBG())
+        return;
+
     Map::Update(diff);
 
     if (!m_bg)
@@ -2508,6 +2518,7 @@ void Map::SendObjectUpdates()
     while (!i_objectsToClientUpdate.empty())
     {
         Object* obj = *i_objectsToClientUpdate.begin();
+
         i_objectsToClientUpdate.erase(i_objectsToClientUpdate.begin());
         obj->BuildUpdateData(update_players);
     }
