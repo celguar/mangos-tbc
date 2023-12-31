f34161ad19ccbe6a877231547197a73dc87eea27
Diabloxx <diabloxx50@gmail.com>
2023-12-31 21:19:23 +0100

    Revert Core Changes
    Testing Object Issue

--- src/game/vmap/GameObjectModel.cpp
+++ src/game/vmap/GameObjectModel.cpp
@@ -168,6 +168,10 @@ bool GameObjectModel::Relocate(GameObject const& go)
     if (!iModel)
         return false;
 
+    if (it->second.name.find(".m2") !
+= std::string::npos)
+        iModel->setModelFlags(VMAP::MOD_M2);
+
     ModelList::const_iterator it = model_list.find(go.GetDisplayId());
     if (it == model_list.end())
         return false;
