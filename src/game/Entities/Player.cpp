74855c5eb0aeda223a23892a02a6fe7e0872d71d
David <davidparraausina@gmail.com>
2024-01-22 16:47:16 +0000

    Make the playerbotai and playerbotmgr unique ptr

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -488,10 +488,6 @@ Player::Player(WorldSession* session): Unit(), m_taxiTracker(*this), m_mover(thi
     m_playerbotAI = 0;
     m_playerbotMgr = 0;
 #endif
-#ifdef ENABLE_PLAYERBOTS
-    m_playerbotAI = 0;
-    m_playerbotMgr = 0;
-#endif
     m_speakTime = 0;
     m_speakCount = 0;
 
@@ -669,6 +665,11 @@ Player::Player(WorldSession* session): Unit(), m_taxiTracker(*this), m_mover(thi
     m_isDebuggingAreaTriggers = false;
 
     m_fishingSteps = 0;
+
+#ifdef ENABLE_PLAYERBOTS
+    m_playerbotAI = nullptr;
+    m_playerbotMgr = nullptr;
+#endif
 }
 
 Player::~Player()
@@ -716,18 +717,8 @@ Player::~Player()
 #endif
 
 #ifdef ENABLE_PLAYERBOTS
-    if (m_playerbotAI) {
-        {
-            delete m_playerbotAI;
-        }
-        m_playerbotAI = 0;
-    }
-    if (m_playerbotMgr) {
-        {
-            delete m_playerbotMgr;
-        }
-        m_playerbotMgr = 0;
-    }
+    RemovePlayerbotAI();
+    RemovePlayerbotMgr();
 #endif
 
     delete m_declinedname;
@@ -1673,12 +1664,35 @@ void Player::Update(const uint32 diff)
 }
 
 #ifdef ENABLE_PLAYERBOTS
+void Player::CreatePlayerbotAI()
+{
+    assert(!m_playerbotAI);
+    m_playerbotAI = std::make_unique<PlayerbotAI>(this);
+}
+
+void Player::RemovePlayerbotAI()
+{
+    m_playerbotAI = nullptr;
+}
+
+void Player::CreatePlayerbotMgr()
+{
+    assert(!m_playerbotMgr);
+    m_playerbotMgr = std::make_unique<PlayerbotMgr>(this);
+}
+
+void Player::RemovePlayerbotMgr()
+{
+    m_playerbotMgr = nullptr;
+}
+
 void Player::UpdateAI(const uint32 diff, bool minimal)
 {
     if (m_playerbotAI)
     {
         m_playerbotAI->UpdateAI(diff, minimal);
     }
+
     if (m_playerbotMgr)
     {
         m_playerbotMgr->UpdateAI(diff);
