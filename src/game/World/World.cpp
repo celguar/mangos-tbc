8a1569880d457afdab7a94c62fa428b93a5f1106
Flekz <davidparraausina@gmail.com>
2024-02-08 20:08:01 +0000

    Merge db

--- src/game/World/World.cpp
+++ src/game/World/World.cpp
@@ -2436,7 +2436,6 @@ void World::UpdateResultQueue()
     CharacterDatabase.ProcessResultQueue();
     WorldDatabase.ProcessResultQueue();
     LoginDatabase.ProcessResultQueue();
-    PlayerbotDatabase.ProcessResultQueue();
 }
 
 void World::UpdateRealmCharCount(uint32 accountId)
